1938212f3da81a467355576b2e65bd7540d4f6c9
cj2k7 <132305566+cj2k7@users.noreply.github.com>
2023-10-04 20:35:53 -0400

    Add files via upload

---
+++ Students.sql
@@ -0,0 +1,49 @@
+SELECT * FROM tiktok.students;
+SELECT
+    AVG(GPA) AS Mean_GPA,
+    STDDEV(GPA) AS StdDev_GPA
+FROM students data;
+SELECT
+    gender,
+    COUNT(*) AS Count
+FROM students data
+GROUP BY gender;
+SELECT
+    class,
+    COUNT(*) AS Count
+FROM students data
+GROUP BY class;
+SELECT
+    class,
+    AVG(GPA) AS Mean_GPA
+FROM students data
+GROUP BY class;
+SELECT
+    gender,
+    AVG(GPA) AS Mean_GPA
+FROM students data
+GROUP BY gender;
+SELECT
+    (
+        SUM(GPA * Algebra) - (SUM(GPA) * SUM(Algebra)) / COUNT(*)
+    ) / (
+        SQRT(
+            (SUM(GPA * GPA) - (SUM(GPA) * SUM(GPA)) / COUNT(*)) *
+            (SUM(Algebra * Algebra) - (SUM(Algebra) * SUM(Algebra)) / COUNT(*))
+        )
+    ) AS Correlation_GPA_Algebra
+FROM students data;
+SELECT
+    CASE
+        WHEN GPA BETWEEN 0 AND 1.60 THEN '0.00 - 1.60'
+        WHEN GPA BETWEEN 1.60 AND 2.20 THEN '1.60 - 2.20'
+        -- Add more cases for other ranges
+    END AS GPA_Range,
+    COUNT(*) AS Count
+FROM students data
+GROUP BY GPA_Range;
+SELECT
+    Class,
+    COUNT(*) AS Count
+FROM students data
+GROUP BY Class;
